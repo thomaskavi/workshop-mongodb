928cb46e3e94f810a147272feee76ab0a4a207ec
thomaskavi <thomaskavi2000@gmail.com>
2025-02-12 13:30:26 -0300

    Get post by id

---
+++ src/main/java/com/thomaskavi/workshopmongo/workshop_mongodb/controllers/PostController.java
@@ -0,0 +1,26 @@
+package com.thomaskavi.workshopmongo.workshop_mongodb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thomaskavi.workshopmongo.workshop_mongodb.models.dto.PostDTO;
+import com.thomaskavi.workshopmongo.workshop_mongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostController {
+
+  @Autowired
+  private PostService service;
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<PostDTO> findById(@PathVariable String id) {
+    PostDTO obj = service.findById(id);
+    return ResponseEntity.ok().body(obj);
+  }
+
+}
---
+++ src/main/java/com/thomaskavi/workshopmongo/workshop_mongodb/services/PostService.java
@@ -0,0 +1,29 @@
+package com.thomaskavi.workshopmongo.workshop_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thomaskavi.workshopmongo.workshop_mongodb.models.dto.PostDTO;
+import com.thomaskavi.workshopmongo.workshop_mongodb.models.entities.Post;
+import com.thomaskavi.workshopmongo.workshop_mongodb.repositories.PostRepository;
+import com.thomaskavi.workshopmongo.workshop_mongodb.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository repository;
+
+  public PostDTO findById(String id) {
+    Post entity = getEntityById(id);
+    return new PostDTO(entity);
+  }
+
+  private Post getEntityById(String id) {
+    Optional<Post> result = repository.findById(id);
+    return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+  }
+
+}
